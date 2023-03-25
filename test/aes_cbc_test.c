2eb5fc8f019805f9710aedcec59715dfe8fdd98f
M3DZIK <me@medzik.dev>
2023-03-25 21:08:47 +0100

    code cleanup add add test  for pbkdf2-sha512

--- test/aes_cbc_test.c
+++ test/aes_cbc_test.c
@@ -1,26 +1,33 @@
 #include "../include/libcrypto.h"
 
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, const char** argv)
+#define CLEAR_TEXT "hello world"
+#define PASSWORD "secret passphrase"
+#define SALT (unsigned char*) "salt"
+#define EXAMPLE_CIPHERTEXT "ceb5156163e045c920cea4748ae302c7e210b4d521925bc342c71145aef3952d"
+
+int main()
 {
-    char *clear_text = "hello world";
-    char *password = "secret passphrase";
-    unsigned char *salt = (unsigned char *) "salt";
+    char *hash = pbkdf2_hash_sha256(PASSWORD, SALT, 1000);
 
-    char *hash = pbkdf2_hash_sha256(password, salt, 1000);
-
-    char *encrypted = aes_cbc_encrypt(clear_text, hash);
-
-    printf("Encrypted: %s\n", encrypted);
-
+    char *encrypted = aes_cbc_encrypt(CLEAR_TEXT, hash);
     char *decrypted = aes_cbc_decrypt(encrypted, hash);
+    if (strcmp(decrypted, CLEAR_TEXT) != 0) {
+        printf("Decrypted text does not match clear text\n");
+        printf("Expected: %s\n", CLEAR_TEXT);
+        printf("Actual: %s\n", decrypted);
+        return 1;
+    }
 
-    printf("Decrypted: %s\n", decrypted);
-
-    char *decrypted_two = aes_cbc_decrypt("ceb5156163e045c920cea4748ae302c7e210b4d521925bc342c71145aef3952d", hash);
-
-    printf("Decrypted: %s\n", decrypted_two);
+    char *decrypted_two = aes_cbc_decrypt(EXAMPLE_CIPHERTEXT, hash);
+    if (strcmp(decrypted_two, CLEAR_TEXT) != 0) {
+        printf("Decrypted text does not match clear text\n");
+        printf("Expected: %s\n", CLEAR_TEXT);
+        printf("Actual: %s\n", decrypted_two);
+        return 1;
+    }
 
     return 0;
 }
--- test/pbkdf2_test.c
+++ test/pbkdf2_test.c
@@ -3,20 +3,28 @@
 #include <stdio.h>
 #include <string.h>
 
+#define PASSWORD "hello world"
+#define SALT (unsigned char*) "salt"
+#define EXPECTED_SHA256 "27426946a796b9a62bc53fba7157961905e4bdd0af2203d6eaf6dd4b64942def"
+#define EXPECTED_SHA512 "883f5fb301ff684a2e92fdfc1754241bb2dd3eb6af53e5bd7e6c9eb2df7ccb7783f40872b5d3dd5c2915a519f008a92c4c2093e8a589e59962cf1e33c8706ca9"
+
 int main()
 {
-    char *password = "hello world";
+    char *hash_sha256 = pbkdf2_hash_sha256(PASSWORD, SALT, 1000);
 
-    unsigned char *salt = (unsigned char *) "salt";
-
-    char *hash = pbkdf2_hash_sha256(password, salt, 1000);
-
-    char *expected = "27426946a796b9a62bc53fba7157961905e4bdd0af2203d6eaf6dd4b64942def";
-
-    if (strcmp(hash, expected) != 0) {
+    if (strcmp(hash_sha256, EXPECTED_SHA256) != 0) {
         printf("hash mismatch\n");
-        printf("Expected: %s\n", expected);
-        printf("Actual: %s\n", hash);
+        printf("Expected: %s\n", EXPECTED_SHA256);
+        printf("Actual: %s\n", hash_sha256);
+        return 1;
+    }
+
+    char *hash_sha512 = pbkdf2_hash_sha512(PASSWORD, SALT, 1000);
+
+    if (strcmp(hash_sha512, EXPECTED_SHA512) != 0) {
+        printf("hash mismatch\n");
+        printf("Expected: %s\n", EXPECTED_SHA512);
+        printf("Actual: %s\n", hash_sha512);
         return 1;
     }
 
